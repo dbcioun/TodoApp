56ec5a7fcf4afd8c9d85a26c30d8e0bafc9dbcd0
dbozacc <dbozacc@live.com>
2017-02-18 23:58:26 -0500

    Testing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
@@ -15,15 +17,14 @@ app.get('/', (req, res) => {
 
 app.post('/todos', (req, res) => {
   let todo = new Todo({
-    text: req.body.text,
-    completed: req.body.completed
+    text: req.body.text
   });
 
   todo.save().then((doc) => {
     res.send(doc)
   }).catch((e) => {
     res.status(400).send(e)
-  });
+  })
 });
 
 app.get('/todos', (req, res) => {
@@ -34,5 +35,40 @@ app.get('/todos', (req, res) => {
   })
 });
 
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id
 
-app.listen('3000', () => console.log("Runnig at port 3000"));
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      res.status(404).send()
+    }
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send()
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send()
+    }
+    res.send(todo)
+  }).catch((e) => {
+    res.status(400).send()
+  })
+});
+
+app.listen(port, () => console.log(`Running at port ${port}`));
+
+module.exports = { app };
